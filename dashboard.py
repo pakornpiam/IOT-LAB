e60545d43f87b773c9f97cf1d34724c801b766c5
pakornpiam <pakornpiam@gmail.com>
2022-12-01 14:15:57 +0700

    update red window

--- dashboard.py
+++ dashboard.py
@@ -3,15 +3,17 @@ from PIL import Image,ImageTk
 GUI = Tk()
 GUI.geometry('900x600')
 GUI.title('DASH BOARD IOT CONTROL SMART FARM')
-## GUI.state('zoomed')  make full screen
+##GUI.state('zoomed')    #make full screen
 
 
 ###test## 1234
-canvas = Canvas(GUI,width=1000,height=900)
+canvas = Canvas(GUI,width=5000,height=900)
 canvas.place(x=0,y=0)
 
 background = ImageTk.PhotoImage(Image.open('farm2.png'))
 canvas.create_image(100,200,anchor=NW,image=background)
 
+#write squar ( polygon ) 
+canvas.create_polygon([378,430,379,380,435,410,435,460],fill='red',outline='black')
 
 GUI.mainloop()
