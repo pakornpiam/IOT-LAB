6103a65b0a72ba7ace39e7e06db820cbf7c24763
pakornpiam <pakornpiam@gmail.com>
2022-12-01 13:33:31 +0700

    update background

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from PIL import Image,ImageTk
 GUI = Tk()
 GUI.geometry('900x600')
 GUI.title('DASH BOARD IOT CONTROL SMART FARM')
@@ -7,6 +7,11 @@ GUI.title('DASH BOARD IOT CONTROL SMART FARM')
 
 
 ###test## 1234
+canvas = Canvas(GUI,width=1000,height=900)
+canvas.place(x=0,y=0)
+
+background = ImageTk.PhotoImage(Image.open('farm2.png'))
+canvas.create_image(100,200,anchor=NW,image=background)
 
 
 GUI.mainloop()
